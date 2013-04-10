5488b15ef70e17f5ba0cb2fff2635763c823d4f0
yubaoquan <1079139460@qq.com>
2013-04-10 12:36:47 +0800

    v1.3.2.1
    更新了一段功能测试代码

--- main.c
+++ main.c
@@ -11,25 +11,25 @@ int  main(){
 	unsigned char array[MAXLEN];
 	unsigned char * fileBeforeEn = calloc(MAXLEN,sizeof(char));
 	unsigned char * fileAfterDe = calloc(MAXLEN,sizeof(char)); 
+	unsigned char * fbecpy;
 	if(fileBeforeEn == NULL || fileAfterDe == NULL){
 		printf("alloc fail!\n");
 		exit(-1);	
 	}else{
 		printf("alloc succeed!\n");
+		fbecpy = fileBeforeEn;
 		char * key = "abcd";
 		
-		fileBeforeEn = "xyz xyz";
+		memcpy(fileBeforeEn,"xyz xyz",8);
 		
 		printf("plaintext: %s\n",fileBeforeEn);
 		des_encipher(fileBeforeEn,fileAfterDe,key);
-
 		printf("ciphertext: %s\n",fileAfterDe);
-		
 		des_decipher(fileAfterDe,array,key);
 		printf("after decrypt: %s \n",array);
-//		free(fileBeforeEn);
- 	//	fileBeforeEn = NULL;
-		printf("plaintext: %s\n",fileBeforeEn);
+		
+		free(fileBeforeEn);
+ 		fileBeforeEn = NULL;
 		free(fileAfterDe);
  		fileAfterDe = NULL;
  		
